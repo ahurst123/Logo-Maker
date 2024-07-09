fceb0ee05496cb99e41ea71ae16980d973657acf
ahurst123 <austinhurst123@gmail.com>
2024-07-08 19:09:00 -0700

    added questions for inquirer, and learned how to validate.

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const { kMaxLength } = require('buffer');
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'Enter a logo name up to three characters long:',
+            validate: input => input.length <= 3 || 'You can only enter logo names up to three characters long'
+        },
+        {
+            type: 'input',
+            name: 'textColor',
+            message: 'Enter the text color with either the color name or hexidecimal code:',
+        },
+        {
+            type: 'list',
+            name: 'shape',
+            message: 'Choose your desired shape:',
+            choices: ['Circle', 'Triangle', 'Square'],
+        },
+        {
+            type: 'input',
+            name: 'shapeColor',
+            message: 'Enter the shape color with either the color name or hexidecimal code:',
+        }
+    ])
