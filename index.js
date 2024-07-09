21e08182a94845c5a1012de93b0ae862b1302066
ahurst123 <austinhurst123@gmail.com>
2024-07-08 19:30:57 -0700

    added shapes and how to make them

--- index.js
+++ index.js
@@ -31,4 +31,16 @@ const generateSVG = ({ text, textColor, shape, shapeColor}) => {
     const svgStart = `<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">`;
     const svgEnd = `</svg>`;
     let shapeElement;
+
+    switch (shape) {
+        case 'circle':
+            shapeElement = `<circle cx="150" cy="100" r="80" fill="${shapeColor}" />`;
+            break;
+        case 'triangle':
+            shapeElement = `<polygon points="150,20 230,180 70,180" fill = "${shapeColor}" />`;
+            break;
+        case 'square':
+            shapeElement = `<rext x="75" y="50" width="150" height="150" fill="${shapeColor}" />`;
+            break;
+    }
 };
