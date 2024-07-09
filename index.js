dd8484a4915b159ce65aff4b2b47d69f528829b2
ahurst123 <austinhurst123@gmail.com>
2024-07-08 20:18:07 -0700

    code doesnt work for me yet, edited the .then statement

--- index.js
+++ index.js
@@ -25,36 +25,35 @@ inquirer
             name: 'shapeColor',
             message: 'Enter the shape color with either the color name or hexidecimal code:',
         }
-    ]);
+    ])
+    .then(answers => {
+        const svgContent = generateSVG(answers);
+        fs.writeFile('logo.svg', svgContent, err => {
+            if (err) {
+                console.error('Error writing your file:', err);
+            } else {
+                console.log('Successfully generated logo.svg!');
+            }
+        }); 
+    });
 
-const generateSVG = ({ text, textColor, shape, shapeColor}) => {
+const generateSVG = ({ text, textColor, shape, shapeColor }) => {
     const svgStart = `<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">`;
     const svgEnd = `</svg>`;
     let shapeElement;
 
-    switch (shape) {
+    switch (shape.toLowerCase()) {
         case 'circle':
             shapeElement = `<circle cx="150" cy="100" r="80" fill="${shapeColor}" />`;
             break;
         case 'triangle':
-            shapeElement = `<polygon points="150,20 230,180 70,180" fill = "${shapeColor}" />`;
+            shapeElement = `<polygon points="150,20 230,180 70,180" fill="${shapeColor}" />`;
             break;
         case 'square':
-            shapeElement = `<rext x="75" y="50" width="150" height="150" fill="${shapeColor}" />`;
+            shapeElement = `<rect x="75" y="50" width="150" height="150" fill="${shapeColor}" />`;
             break;
     }
-    const textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}<text>`;
+    const textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>`;
 
     return `${svgStart}${shapeElement}${textElement}${svgEnd}`;
 };
-
-inquirer.prompt(questions).then(answers => {
-    const svgContent = generateSVG(answers);
-    fs.writeFile('logo.svg', svgContent, err => {
-        if (err) {
-            console.error('Error writng your file:', err);
-        } else {
-            console.log('Successfully generated logo.svg!');
-        }
-    }); 
-});
